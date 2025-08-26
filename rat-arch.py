28635a4f9d5de51d308086bbadd75c477316b6ca
Riven-py <mauribencollege@gmail.com>
2025-08-26 18:48:08 +0800

    add: Install script

---
+++ rat-arch.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+su = "sudo"
+
+processes = [
+            "sudo pacman -Sy",
+            
+            ]
+
+for process in processes:
+    result = subprocess.run(process,shell=True)
+    if result != 0:
+        print(f"{process} failed. exit code {result}")
+        break
+
+
+
